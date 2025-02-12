4a6ff934731c661d2b35ce11d9bee6a37e046e3c
soonhankwon <soonable@gmail.com>
2025-02-12 16:19:33 +0900

    update config

--- tool/fix-image-references.sh
+++ tool/fix-image-references.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-IS_CORRECT_WORKING_DIRECTORY=`pwd | egrep "johngrib.github.io$" | wc -l`
+IS_CORRECT_WORKING_DIRECTORY=`pwd | egrep "soonhankwon.github.io$" | wc -l`
 if (( "$IS_CORRECT_WORKING_DIRECTORY" != 1 )); then
-    echo "올바른 경로가 아닙니다. johngrib.github.io/ 에서 실행해 주세요."
+    echo "올바른 경로가 아닙니다. soonhankwon.github.io/ 에서 실행해 주세요."
     exit;
 fi
 
